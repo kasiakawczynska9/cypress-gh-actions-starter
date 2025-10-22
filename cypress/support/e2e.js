6d65bf861ee21de8f36b0aa8fefffb78e14545a2
Kasia Kawczynska <kkawczynska3@gmail.com>
2025-10-22 20:27:01 +0100

    Fix grep import to avoid webpack error

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,3 +1,4 @@
 import 'cypress-mochawesome-reporter/register';
-// enable cypress-grep (tagi, np. @smoke)
-require('@cypress/grep')();
+import registerCypressGrep from '@cypress/grep';
+
+registerCypressGrep();
